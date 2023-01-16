84ab88ccde3458bdb8ac31d7e83d371b96c5bd4e
Bruno Andrade <brunoandrade.contato@icloud.com>
2023-01-16 10:47:57 -0300

    feat: artist model and migration

---
+++ api/models/artist.ts
@@ -0,0 +1,54 @@
+'use strict';
+
+import { Model, UUIDV4 } from 'sequelize'
+
+interface ArtistAttributes {
+	id: number,
+	title: string,
+	mainGenre: string
+	avatar?: string,
+	bio?: string
+}
+
+module.exports = (sequelize: any, DataTypes: any) => {
+	class Artists extends Model<ArtistAttributes> implements ArtistAttributes {
+		id: number;
+		title: string;
+		mainGenre: string;
+		avatar?: string;
+		bio?: string;
+
+		static associate(models: any) {
+			// define association here
+		}
+	}
+
+	Artists.init({
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: UUIDV4,
+			primaryKey: true,
+			allowNull: false
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		mainGenre: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		avatar: {
+			type: DataTypes.STRING,
+			allowNull: true,
+		},
+		bio: {
+			type: DataTypes.STRING,
+			allowNull: true
+		}
+	}, {
+		sequelize,
+		modelName: 'Artists',
+	});
+	return Artists;
+};
