b2e0289072eae3f0514ecfb2c4ecfb66fdb6abf6
Bruno Andrade <brunoandrade.contato@icloud.com>
2023-01-16 15:02:26 -0300

    feat: removed src with javascript version

--- src/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-const ArtistResolver = require('./artistsResolver')
-const DiscoResolver = require('./discosResolver')
-
-const resolvers = [
-	ArtistResolver,
-	DiscoResolver
-]
-
-module.exports = resolvers
